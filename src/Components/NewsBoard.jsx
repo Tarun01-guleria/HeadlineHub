07773f835b064ab60618ffee120b0d780182fa62
Tarun Singh Guleria <guleriatarun672@gmail.com>
2025-01-09 00:11:10 +0530

    Intial commit

---
+++ src/Components/NewsBoard.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import NewsItem from "./NewsItem";
+
+const NewsBoard = ({ category }) => {
+  const [articles, setArticles] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${
+      import.meta.env.VITE_API_KEY
+    }`;
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => setArticles(data.articles || []))
+      .catch((error) => setError(error.message));
+  }, [category]);
+
+  return (
+    <div>
+      <h2 className="text-center">
+        Latest <span className="badge bg-danger">News</span>
+      </h2>
+      {error ? (
+        <p>Error: {error}</p>
+      ) : (
+        articles.map((news, index) => (
+          <NewsItem
+            key={index}
+            title={news.title}
+            description={news.description}
+            src={news.urlToImage}
+            url={news.url}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default NewsBoard;
